2883a9ded5af503e799f144f3856eecf24e3e1df
dovekie <dovekie@gmail.com>
2015-10-05 14:47:53 -0700

    Draw and transform a battery symbol

--- static/circ.js
+++ static/circ.js
@@ -16,4 +16,20 @@ window.onload = function() {
 
     resistor.animate({transform: "r" + 90 + "," + resX + "," + resY}, 1000);
 
+    var batX = 300;
+    var batY = 300;
+
+    var battery = paper.set (
+        paper.path("M " + batX + " " + batY + " l 0 50"), // top vertical
+        paper.path("M " + (batX-50) + " " + (batY+50) + " l 100 0"), // top long bar
+        paper.path("M " + (batX-20) + " " + (batY+65) + " l 40 0"), // middle short bar
+        paper.path("M " + (batX-50) + " " + (batY+80) + " l 100 0"), // middle long bar
+        paper.path("M " + (batX-20) + " " + (batY+95) + " l 40 0"), // bottom short bar
+        paper.path("M " + batX + " " + (batY+95) + " l 0 50") // bottom vertical
+    );
+
+    battery.attr({'stroke-width': 5, 'stroke-linejoin': 'round'});
+    battery.animate({transform: "r" + -90 + "," + batX + "," + batY}, 1000);
 }
+
+// M -75 5 l 25 0
