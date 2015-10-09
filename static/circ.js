6323916d1583762f0cfac83f0ae7825d5da2cea7
dovekie <dovekie@gmail.com>
2015-10-08 19:52:03 -0700

    Moving resistors into a function

--- static/circ.js
+++ static/circ.js
@@ -1,11 +1,47 @@
 // script for drawing circuits
 
-// circuit elements are 140 long and 50 wide (resistors are 40 wide + margins; battery has no margins)
+// circuit elements are 140 long and 50 wide (resistors are 40 wide + margins; battery is 25 wide + margins)
 // elements should be separated by at least 100 (for 50 clearance from edge to edge)
 
-var numResistors = 4;
+Raphael.fn.resistorGroup = function(batX, batY, numResistors, relationship) { 
+    /* Build a set of resistors
+       Takes the battery's X and Y coords, the number of resistors in the group,
+       And a string that is either "series" or "parallel"
+       Returns a Raphael set.
+    */
+    this.relationship = relationship;
+    this.numResistors = numResistors;
+    this.resX = batX + 100;
+    if (this.relationship === "series") {
+        this.resY = batY - 70;
+    } else {
+        this.resY = batY;
+    }
 
-window.onload = function() {
+    var Group = this.set();
+
+    for (var i = 0; i < numResistors; i++) {
+        var newResistor = this.set(
+            this.circle(this.resX, this.resY, 5)
+                .attr({'fill': '#000000'}),
+            this.circle(this.resX + 140, this.resY, 5)
+                .attr({'fill': '#000000'}),
+            this.path("M " + this.resX + " " + this.resY + " l 24 0 l 9 20 l 18 -40 l 17 40 l 18 -40 l 18 40 l 9 -20 l 24 0")
+                .attr({'stroke-width': 5, 'stroke-linejoin': 'round'})
+            );
+
+        if (this.relationship === "series") {
+            newResistor.transform("r90," + this.resX + "," + this.resY);
+        }
+
+        Group.push(newResistor);
+
+        this.resY = this.resY + 140;
+    } 
+    return Group;
+}
+
+window.onload = function () {
     var paper = new Raphael(document.getElementById('canvas_container'), 1000, 1000);
     var batX = 100;
     var batY = 100;
@@ -23,63 +59,9 @@ window.onload = function() {
 
     battery.attr({'stroke-width': 5, 'stroke-linejoin': 'round'});
 
-    var relationship = "parallel";
-    var parallelGroup = paper.set();
-    var resX = batX + 100;
-    if (relationship === "parallel") {
-        var resY = batY - 70;
-    } else {
-        var resY = batY;
-    }
+    var res1 = paper.resistorGroup(batX, batY, 2, "series");
 
-    for (var i = 0; i < numResistors; i++) {
-        var newResistor = paper.set(
-            paper.circle(resX, resY, 5)
-                .attr({'fill': '#000000'}),
-            paper.circle(resX + 140, resY, 5)
-                .attr({'fill': '#000000'}),
-            paper.path("M " + resX + " " + resY + " l 24 0 l 9 20 l 18 -40 l 17 40 l 18 -40 l 18 40 l 9 -20 l 24 0")
-                .attr({'stroke-width': 5, 'stroke-linejoin': 'round'})
-            );
-
-        if (relationship === "series") {
-            newResistor.transform("r90," + resX + "," + resY);
-        }
-
-        parallelGroup.push (
-            newResistor
-        );
-
-        resY = resY + 140;
-    } 
-
-    // var resX = 100;
-    // var resY = 100;
-
-    // var parallelGroup = paper.set()
-
-    // for (var i = 0; i < numResistors; i++) {
-    //      resY = resY + (i * 100);
-    //      parallelGroup.push (
-    //         paper.circle(resX, resY, 5)
-    //             .attr({'fill': '#000000'}),
-    //         paper.circle(resX + 140, resY, 5)
-    //             .attr({'fill': '#000000'}),
-    //         paper.path("M " + resX + " " + resY + " l 24 0 l 9 20 l 18 -40 l 17 40 l 18 -40 l 18 40 l 9 -20 l 24 0")
-    //             .attr({'stroke-width': 5, 'stroke-linejoin': 'round'})//,
-    //         paper.path("M " + (resX + (i * 140)) + " " + (resY - (i * 100)) + " l 0 100")
-    //             .attr({'stroke-width': 5, 'stroke-linejoin': 'round'})
-    //         );
-    // } // this ends your for loop
-
-    // parallelGroup.transform("r90,"+ (100 + (145/2)) + "," + (100 + (resY/4)) + ",t23,-250");
-
-
-
-    // var wire1 = paper.path("M" + batX + " " + batY + "L370 100").attr({'stroke-width': 5, 'stroke-linejoin': 'round'});
-
-    // var wire2 = paper.path("M" + batX + " " + (batY + 145) + "L370 245").attr({'stroke-width': 5, 'stroke-linejoin': 'round'});
-
+    var res2 = paper.resistorGroup(batX+100, batY+70, 1, "series");
 }
 
 // how to animate! for reference.
